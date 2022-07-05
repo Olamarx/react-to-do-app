4b2789e8685f5f5f09f71570a8c3ea54901a671c
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-07-05 04:26:57 +0100

    Made the toggle function work

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -11,7 +11,7 @@ class TodoItem extends React.Component {
         <li>
             <input type="checkbox"
             checked={this.props.todo.completed}
-            onChange={() => this.props.handleChangeProps()}
+            onChange={() => this.props.handleChangeProps(this.props.todo.id)}
             />
             {this.props.todo.title}
         </li>
