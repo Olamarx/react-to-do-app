e8d8e6dd7f25e9858b284ee94c8736ff1bb775c6
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-07-05 07:11:46 +0100

    Fix certainer linters but not all.

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -4,6 +4,7 @@ import Header from './Header';
 import InputTodo from './InputTodo';
 import './TodoContainer.css';
 import { v4 as uuidv4 } from 'uuid';
+
 class TodoContainer extends React.Component {
   constructor(props) {
     super(props);
@@ -29,53 +30,51 @@ class TodoContainer extends React.Component {
   }
 
   handleChange = (id) => {
-    this.setState(prevState => ({
-        todos: prevState.todos.map(todo => {
-            if (todo.id === id) {
-             return {
-                ...todo,
-                completed: !todo.completed,
-             }
-            }
-            return todo
-        })
-    }))
+    this.setState((prevState) => ({
+      todos: prevState.todos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+        return todo;
+      }),
+    }));
   }
 
-  delTodo = id => {
+  delTodo = (id) => {
     this.setState({
-        todos: [
-            ...this.state.todos.filter(todo => {
-                return todo.id !== id;
-            })
-        ]
-    })
+      todos: [
+        ...this.state.todos.filter((todo) => todo.id !== id),
+      ],
+    });
   };
 
-  addTodoItem = title => {
+  addTodoItem = (title) => {
     const newTodo = {
-        id: uuidv4(),
-        title: title,
-        completed: false
-    }
+      id: uuidv4(),
+      title,
+      completed: false,
+    };
     this.setState({
-        todos: [...this.state.todos, newTodo]
-    })
+      todos: [...this.state.todos, newTodo],
+    });
   };
 
   render() {
     return (
-        <div className="container">
-            <div className="inner">
-            <Header />
-            <InputTodo addTodoProps={this.addTodoItem} />
-            <TodosList
+      <div className="container">
+        <div className="inner">
+          <Header />
+          <InputTodo addTodoProps={this.addTodoItem} />
+          <TodosList
             todos={this.state.todos}
             handleChangeProps={this.handleChange}
             deleteTodoProps={this.delTodo}
-            />
-            </div>
+          />
         </div>
+      </div>
     );
   }
 }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import style from './TodoItem.module.css'
+import style from './TodoItem.module.css';
+
 class TodoItem extends React.Component {
   constructor(props) {
     super(props);
@@ -8,26 +9,30 @@ class TodoItem extends React.Component {
 
   render() {
     const completedStyle = {
-      fontStyle: "italic",
-      color: "#595959",
+      fontStyle: 'italic',
+      color: '#595959',
       opacity: 0.4,
-      textDecoration: "line-through",
-    }
+      textDecoration: 'line-through',
+    };
     return (
-        <li className={style.item}>
-            <input type="checkbox"
-            className={style.checkbox}
-            checked={this.props.todo.completed}
-            onChange={() => this.props.handleChangeProps(this.props.todo.id)}
-            />
-             <span style={this.props.todo.completed ? completedStyle : null}>
-      {this.props.todo.title}
-    </span>
-            <button
-            onClick={() => this.props.deleteTodoProps(this.props.todo.id)}
-            >Delete</button>
-        </li>
-  )}
+      <li className={style.item}>
+        <input
+          type="checkbox"
+          className={style.checkbox}
+          checked={this.props.todo.completed}
+          onChange={() => this.props.handleChangeProps(this.props.todo.id)}
+        />
+        <span style={this.props.todo.completed ? completedStyle : null}>
+          {this.props.todo.title}
+        </span>
+        <button
+          onClick={() => this.props.deleteTodoProps(this.props.todo.id)}
+        >
+          Delete
+        </button>
+      </li>
+    );
+  }
 }
 
 TodoItem.propTypes = {
